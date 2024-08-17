ed1ade4c34b7572869cbd8ab421ad0d6b106397d
Gabriel Freire <gabrielmfreire2506@gmail.com>
2024-08-16 23:00:30 -0300

    Criação interface principal

---
+++ frontend/scripts/script.js
@@ -0,0 +1,11 @@
+//<button class="button" id="bt_ini">Página Inicial</button>
+//<button class="button" id="bt_calen">Calendário</button>
+//<button class="button" id="bt_ativ">Aprovar Atividades</button>
+//<button class="button" id="bt_sair">Sair</button>
+
+
+const bt1 = document.getElementById("bt_ini")
+bt1.addEventListener("click",(e)=>{
+    bt1.style.backgroundColor="#fff"
+}
+)
