665dbb2cac2d50488b0df996c6ea6a056c846589
Gabriel Freire <gabrielmfreire2506@gmail.com>
2024-08-18 19:17:58 -0300

    atualização js

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -5,7 +5,59 @@
 
 
 const bt1 = document.getElementById("bt_ini")
-bt1.addEventListener("click",(e)=>{
-    bt1.style.backgroundColor="#fff"
+bt1.addEventListener("click",redirecionar);
+
+function redirecionar(){
+    window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+
+
 }
-)
+
+
+const bt2 = document.getElementById("bt_calen")
+bt2.addEventListener("click",redirecionar);
+
+function redirecionar(){
+        window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+}
+
+const bt3 = document.getElementById("bt_ativ")
+bt3.addEventListener("click",redirecionar);
+function redirecionar(){
+    window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+
+}
+const bt4 = document.getElementById("bt_sair")
+bt4.addEventListener("click",redirecionar);
+function redirecionar(){
+    window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+}
+
+
+
+const bot1 = document.getElementById("bot1")
+bot1.addEventListener("click",redirecionar);
+    function redirecionar(){
+        window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+}
+
+
+const bot2 = document.getElementById("bot2")
+bot2.addEventListener("click",redirecionar);
+    function redirecionar(){
+        window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+}
+
+
+const bot3 = document.getElementById("bot3")
+bot3.addEventListener("click",redirecionar);
+    function redirecionar(){
+        window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+}
+
+
+const bot4 = document.getElementById("bot4")
+bot4.addEventListener("click",redirecionar);
+    function redirecionar(){
+        window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+}
