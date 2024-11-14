bc9d63165975c910a3ed7ee3162729c578d98140
ridoolfi <angeloggc2@gmail.com>
2024-11-14 20:57:03 -0300

    CORS TESTE

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,9 +8,9 @@ const cookieParser = require('cookie-parser');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
 app.use('/', routers)
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors({ origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(cookieParser());
 
 
